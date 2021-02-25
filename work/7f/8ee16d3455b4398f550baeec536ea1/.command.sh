98e5feb181b25b7a213f1e2d86218b76cab63461
Flip Mulder <f.mulder@umcutrecht.nl>
2021-02-25 14:15:16 +0100

    Test commit retry

--- work/7f/8ee16d3455b4398f550baeec536ea1/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-blastn  -num_threads 2 -db /mnt/d/Work/nextflow_stuff/nftutorial/DB/blastDB -query /mnt/d/Work/nextflow_stuff/nftutorial/input.fasta -outfmt 6 -out input.blastout
--- work/c9/0188819dd55c40e3180e3fa3a02541/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-blastn  -num_threads 2 -db /mnt/d/Work/nextflow_stuff/nftutorial/DB//blastDB -query input.1.fasta -outfmt '6' -evalue 1e-3 -out input.blastout
